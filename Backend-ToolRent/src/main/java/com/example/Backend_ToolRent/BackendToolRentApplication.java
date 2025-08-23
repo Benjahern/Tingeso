c04d0af382852945a982c1778529f183c7be4bfb
BenjaHern <benjamin.hernandez.me@usach.cl>
2025-08-22 21:50:59 -0400

    first commit

---
+++ Backend-ToolRent/src/main/java/com/example/Backend_ToolRent/BackendToolRentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Backend_ToolRent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendToolRentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendToolRentApplication.class, args);
+	}
+
+}
---
+++ Backend-ToolRent/src/test/java/com/example/Backend_ToolRent/BackendToolRentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Backend_ToolRent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendToolRentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
